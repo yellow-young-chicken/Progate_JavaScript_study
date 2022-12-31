f551a915e63f8de4f2a8a9bdac87d2c0aae37278
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2022-12-31 14:34:14 +0000

    done2-6

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ const age = 17;
 // 条件式が成り立たない場合に「私は20歳未満です」と出力してください
 if (age >= 20) {
   console.log("私は20歳以上です");
-} 
+}
 else {
   console.log("私は20歳未満です");
 }
@@ -140,7 +140,7 @@ const age = 17;
 // ageの値が10以上20未満のとき、「私は20歳未満ですが、10歳以上です」と出力してください
 if (age >= 20) {
   console.log("私は20歳以上です");
-} 
+}
 else if (age >= 10) {
   console.log("私は20歳未満ですが、10歳以上です");
 }
@@ -170,7 +170,7 @@ switch (n) {
   case 2:
     console.log("吉です");
     break;
-  
+
 
   // nの値が3のcaseを追加してください
   case 3:
@@ -196,3 +196,67 @@ switch (n) {
     console.log("凶です");
     break;
 }
+
+// 変数numberを定義してください
+let number = 0 ;
+
+// 変数numberに1を加えて、その後に変数numberの値を出力してください
+
+number += 1;
+console.log(number);
+
+// 上述の2行のコードを4回、貼り付けてください
+
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+// 変数numberを定義してください
+let number = 1 ;
+
+// while文を作成してください
+
+while(number<=100){
+  console.log(number);
+  number += 1;
+}
+
+// for文を用いて、1から100までの数字を出力してください
+for (let number = 1; number <= 100; number++) {
+  console.log(number);
+}
+
+// for文を完成させてください
+for (let number = 1;number<= 100 ;number ++) {
+  // if文を用いて、numberが3の倍数の時に「3の倍数です」、そうでないときは数字を出力してください
+  if(number % 3 == 0){
+    console.log("3の倍数です");
+  }
+  else {
+     console.log(number); 
+  }
+}
+
+// 定数animalsに、指定された配列を代入してください
+const animals = ["dog", "cat", "sheep"];
+
+// 定数animalsを出力して下さい
+console.log(animals);
+
+const animals = ["dog", "cat", "sheep"];
+
+// 配列の1つ目の要素を出力してください
+console.log(animals[0]);
+
+// 配列の3つ目の要素を出力してください
+
+console.log(animals[2]);
