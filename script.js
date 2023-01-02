311bb0aa1b18314825f8b8781d78357aeaff0285
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-01-02 13:48:17 +0000

    1/2

--- script.js
+++ script.js
@@ -531,3 +531,13 @@ const result = toMinutes(3,20);
 // 「◯◯分」となるように、分に換算した結果を出力してください
 
 console.log(`${result}分`);
+
+const characters = ["にんじゃわんこ", "ベイビーわんこ", "ひつじ仙人"];
+
+console.log(characters);
+
+// pushメソッドを使って配列charactersに、文字列「とりずきん」を追加してください
+characters.push("とりずきん");
+
+// 配列charactersを出力してください
+console.log(characters);
