7bbbbc2d12053ae0acafe3527c698b635c187d94
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-01-04 10:20:04 +0000

    done 4

--- script.js
+++ script.js
@@ -633,5 +633,57 @@ const fullNames = names.map((name)=>{
 // 定数fullNamesを出力してください
 console.log(fullNames);
 
+const printWanko = () => {
+  console.log("にんじゃわんこ");
+};
 
-git test
+const printHitsuji = () => {
+  console.log("ひつじ仙人");
+};
+
+const call = (callback) => {
+  console.log("コールバック関数を呼び出します。");
+  callback();
+};
+
+// 引数をprintHitsujiに書き換えて出力を確認しましょう
+call(printHitsuji);
+
+const printWanko = () => {
+  console.log("にんじゃわんこ");
+};
+
+// 関数callにcallbackという名前の引数を追加してください
+const call = (callback) => {
+  console.log("コールバック関数を呼び出します。");
+  // 引数に渡したcallbackを呼び出してください
+  callback();
+};
+
+// 関数printWankoを引数に渡して関数callを実行してください
+
+call(printWanko);
+const printWanko = () => {
+  console.log("にんじゃわんこ");
+};
+
+const call = (callback) => {
+  console.log("コールバック関数を呼び出します。");
+  callback();
+};
+
+call(printWanko);
+
+// 引数で関数を定義して渡してください
+call(() => {
+  console.log("ひつじ仙人");
+});
+
+const call = (callback) => {
+  callback("にんじゃわんこ", 14);
+};
+
+// 関数callの引数の中で2つの引数を取る関数を追加してください
+call((name,age) => {
+  console.log(`${name}は${age}歳です。`);
+});
