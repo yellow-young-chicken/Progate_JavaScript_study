b46aab8ab65552444593b00cb6d68601a7d27da0
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-01-24 14:56:53 +0000

    again 2-7

--- script.js
+++ script.js
@@ -913,3 +913,71 @@ switch (n) {
 
 
 }
+// 変数numberを定義してください
+let number = 0 ;
+
+// 変数numberに1を加えて、その後に変数numberの値を出力してください
+
+number += 1;
+console.log(number);
+
+// 上述の2行のコードを4回、貼り付けてください
+
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+
+// 変数numberを定義してください
+let number = 1 ;
+
+// while文を作成してください
+
+while(number<=100){
+  console.log(number);
+  number += 1;
+}
+
+// for文を用いて、1から100までの数字を出力してください
+
+for(let number=1; number<=100;number++){
+  console.log(number);
+}
+
+
+// for文を完成させてください
+for (let number = 1; number<= 100; number ++){
+  // if文を用いて、numberが3の倍数の時に「3の倍数です」、そうでないときは数字を出力してください
+  
+  if (number % 3 === 0){
+    console.log("3の倍数です");
+  }
+  else{
+    console.log(number); 
+  }
+}
+
+
+// 定数animalsに、指定された配列を代入してください
+const animals = ["dog","cat","sheep"];
+
+// 定数animalsを出力して下さい
+
+console.log(animals);
+
+const animals = ["dog", "cat", "sheep"];
+
+// 配列の1つ目の要素を出力してください
+console.log(animals[0]);
+
+// 配列の3つ目の要素を出力してください
+console.log(animals[2]);
