36537ed173379eac586caa25e50150345c0ccec0
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-01-31 16:33:11 +0000

    2-9

--- script.js
+++ script.js
@@ -1000,3 +1000,14 @@ const animals = ["dog", "cat", "sheep"];
 for(let i = 0; i<3; i ++){
   console.log(animals[i]);
 }
+
+
+const animals = ["dog", "cat", "sheep", "rabbit", "monkey", "tiger", "bear", "elephant"];
+
+// lengthを用いて配列の要素の数を出力してください
+console.log(animals.length);
+
+// lengthを用いて条件式を書き換えてください
+for (let i = 0; i<animals.length; i++) {
+  console.log(animals[i]);
+}
