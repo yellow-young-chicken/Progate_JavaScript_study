da1e092bb46f1536122a66d3e383d8463ba5e40a
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-02-11 18:38:01 +0000

    2-13

--- script.js
+++ script.js
@@ -1042,3 +1042,29 @@ console.log(characters[0]);
 
 // charactersの2つ目の要素の「name」に対応する値をコンソールに出力してください
 console.log(characters[1].name);
+
+const characters = [
+  {name: "にんじゃわんこ", age: 14},
+  {name: "ひつじ仙人", age: 100},
+  {name: "ベイビーわんこ", age: 5},
+];
+
+// for文を完成させてください
+for (let i = 0; i < characters.length; i++) {
+  console.log("--------------------");
+  
+  // 定数characterを定義してください
+  const character = characters[i];
+  
+
+  // 「名前は〇〇です」を出力してください
+  console.log(`名前は${character.name}です`);
+  
+
+  // 「〇〇歳です」を出力してください
+  console.log(`${character.age}歳です`);
+ 
+}
+
+
+
